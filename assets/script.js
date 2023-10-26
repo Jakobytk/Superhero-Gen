4cc7478ca040c04fefe4077c94689e6fffca50dc
Jacob Kueck <jacob.kueck@gmail.com>
2023-10-25 20:29:15 -0500

    html and javascript improve

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 // variable for search button
-var submitBtn = document.getElementById('');
+var submitBtn = document.getElementById('submitBtn');
 
 
 function getParams() {
   // Get the queryvalues
-  var query = document.getElementById('').value;
+  var query = document.getElementById('userInput').value;
 
   searchApi(query);
 }
@@ -12,11 +12,12 @@ function getParams() {
 // this function takes query argument and changes wiki url 
 // then returns data on each api
 function searchApi(query) {
-  var wikiQueryUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=hell';
-  var giphyQueryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=2Aq30axh0Bdf37VdoDjBnkiUJRXocruK&q=hero&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
+  var wikiQueryUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=5&srsearch=';
+  var giphyQueryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=2Aq30axh0Bdf37VdoDjBnkiUJRXocruK&q=hero&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
 
   if (query) {
-    wikiQueryUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=' + query;
+    wikiQueryUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=5&srsearch=' + query;
+    giphyQueryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=2Aq30axh0Bdf37VdoDjBnkiUJRXocruK&q='+query+'&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
   }
 
   fetch(wikiQueryUrl)
